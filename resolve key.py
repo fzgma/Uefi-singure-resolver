c51a6a71740020cccd7bb19baf83af89eb95bd90
fzgma <fzgma@users.noreply.github.com>
2025-12-11 15:02:05 +0800

    resolver

--- resolve key.py
+++ resolve key.py
@@ -1,3 +1,7 @@
+#UEFI Secure Boot database resolver.
+#Parses PK/KEK/DB/DBX signature lists and exports certificate info.
+
+
 import struct
 import uuid
 from cryptography import x509
@@ -135,4 +139,3 @@ output_file = "UEFI_SecureBoot_entries.xlsx"
 wb.save(output_file)
 
 print(f"Excel 文件已生成：{output_file}")
-
